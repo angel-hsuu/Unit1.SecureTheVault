7cf176c80243d6db6e0d99a1618e02473e64c9c5
Angel <angelhsu17@gmail.com>
2025-06-11 10:18:39 -0700

    SecuretheVault

---
+++ index.js
@@ -0,0 +1,18 @@
+let code1;
+code1 = 7+3;
+
+let code2 = (100 - 20) / 2;
+
+let code3 = 100 % 61;
+
+let message;
+message = "The vault has been secured. The combination is:";
+
+let codeA;
+codeA = code1 + "-" + code2 + "-" + code3;
+let codeB;
+codeB = `${code1}-${code2}-${code3}`;
+
+console.log(message);
+console.log(codeA);
+console.log(codeB);
